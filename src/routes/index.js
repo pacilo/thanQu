f51147c51f82da764db7090bf53714b2889042d2
치치 <ACE>
2017-05-28 16:49:59 +0900

    다시 수정함

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ router.post('/class', function(req, res, next){
   res.render('className', {classname :  req.body.clname});
 });
 
-router.post('/room', function(req, res, next){
+router.get('/room', function(req, res, next){
   res.render('realClass');
 });
 
