0d2245850d28f749db91a946f6e22e2818ecfc71
pacilo <pacilo93@gmail.com>
2017-06-05 05:24:05 +0900

    aws 임포트 제거

--- src/routes/restful.js
+++ src/routes/restful.js
@@ -3,8 +3,8 @@ var router = express.Router();
 const fs = require('fs');
 const ejs = require('ejs');
 const pool = require('../config/db_pool');
-var aws = require('aws-sdk');
-aws.config.loadFromPath('./config/aws_config.json');
+// var aws = require('aws-sdk');
+// aws.config.loadFromPath('./config/aws_config.json');
 
 router.post('/join', function(req, res, next){
 
